86794a1039a7e73cfdfc775b43fdf97ea013aa22
yunjaeeun <yunjaeeun12@naver.com>
2025-01-20 09:20:41 +0900

    프로젝트 기본 세팅

---
+++ meeple_back/src/main/java/com/meeple/meeple_back/MeepleBackApplication.java
@@ -0,0 +1,13 @@
+package com.meeple.meeple_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeepleBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeepleBackApplication.class, args);
+    }
+
+}
