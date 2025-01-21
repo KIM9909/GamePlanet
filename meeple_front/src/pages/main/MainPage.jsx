bc7587f1f76058c670206bb069e3c013ac531a71
kimhongbeom <fkdldj48@naver.com>
2025-01-21 22:30:46 +0900

    ✨feat: [FE] 회원가입, 로그아웃 기능

--- meeple_front/src/pages/main/MainPage.jsx
+++ meeple_front/src/pages/main/MainPage.jsx
@@ -1,10 +1,23 @@
-import { useDispatch } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import React from "react"
-import { setModalOpen } from "../../sources/api/store/slices/UserSlice"
+import { setModalOpen, logout } from "../../sources/api/store/slices/UserSlice"
 import LoginModal from "../../components/user/LoginModal"
+import { useState } from "react"
+import RegisterModal from "../../components/user/RegisterModal"
 
 const MainPage = () => {
   const dispatch = useDispatch()
+  // 회원가입 모달의 표시 상태 관리
+  const [isRegisterModalOpen, setIsRegisterModalOpen] = useState(false)
+
+  // Redux store에서 token 상태를 가져옴 (로그인 상태 확인용)
+  const { token } = useSelector((state) => state.user)
+  
+  // 로그아웃 처리 핸들러
+  // 리덕스의 logout 액션을 디스패치하여 토큰 제거거
+  const handleLogout = () => {
+    dispatch(logout())
+  }
 
   return (
     <div className="min-h-screen bg-gray-100">
@@ -13,13 +26,40 @@ const MainPage = () => {
           <h1 className="text-3xl font-bold text-gray-900">Meeple</h1>
         </div>
       </header>
-      <button
-        onClick={() => dispatch(setModalOpen(true))}
-        className="px-4 py-2 bg-blue-500 text-white rouded-md hover:bg-blue-600"
-      >
-        로그인
-      </button>
-      <LoginModal />
+
+      <div className="flex justify-center">
+        {!token ? (
+          // 로그인하지 않은 경우
+          <>
+            <button
+              onClick={() => dispatch(setModalOpen(true))}
+              className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+            >
+              로그인
+            </button>
+            <button 
+              onClick={() => setIsRegisterModalOpen(true)}
+              className="ml-5 px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600"
+            >
+              회원가입
+            </button>
+          </>
+        ) : (
+          // 로그인된 경우
+          <button
+            onClick={handleLogout}
+            className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
+          >
+            로그아웃
+          </button>
+        )}
+
+        <LoginModal />
+        <RegisterModal
+          isOpen={isRegisterModalOpen}
+          onClose={() => setIsRegisterModalOpen(false)}
+        />
+      </div>
     </div>
   )
 }
--- meeple_front/src/sources/api/store/Store.jsx
+++ meeple_front/src/sources/api/store/Store.jsx
@@ -4,11 +4,14 @@ import boardReducer from './slices/BoardSlice'
 import gameReducer from './slices/GameSlice'
 import tournamentReducer from './slices/TournamentSlice'
 
+// Redux 스토어 생성 및 설정
 export const Store = configureStore({
+  // 루트 리듀서 설정
   reducer: {
-    user: userReducer,
-    board: boardReducer,
-    game: gameReducer,
-    tournament: tournamentReducer
+    user: userReducer, // 유저 관련 상태 관리
+    board: boardReducer, // 게시판 관련 상태 관리
+    game: gameReducer, // 게임 관련 상태 관리
+    tournament: tournamentReducer // 토너먼트 관련 상태 관리
   }
+  // 기본적으로 Redux DevTools와 Redux Thunk가 포함됨
 })
