890b74d8a31effe9ae85b29235035beefdba930b
seonghyunKim <aruesin2@gmail.com>
2025-01-21 22:38:10 +0900

    :sparkles:Feat(user): user repository 테스트 코드 추가

--- meeple_back/src/test/resources/user-repository-test-data.sql
+++ meeple_back/src/test/resources/user-repository-test-data.sql
@@ -1,17 +1,21 @@
+-- 스키마 생성
 CREATE SCHEMA IF NOT EXISTS GAME_PROJECT;
+
+-- 스키마 설정
 SET SCHEMA GAME_PROJECT;
 
-CREATE TABLE `tbl_user` (
-                            `user_id`	BIGINT	NOT NULL primary key AUTO_INCREMENT,
-                            `user_name`	BIGINT	NOT NULL ,
-                            `user_email`	BIGINT	NOT NULL UNIQUE ,
-                            `user_password`	VARCHAR(255)	NOT NULL,
-                            `user_birthday`	DATE	NOT NULL,
-                            `user_nickname`	VARCHAR(50)	NOT NULL,
-                            `user_profile_picture_url`	VARCHAR(500)	NULL,
-                            `user_tier`	VARCHAR(20)	NULL,
-                            `user_level`	INT	NULL,
-                            `user_created_at`	TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-                            `user_updated_at`	TIMESTAMP	NULL,
-                            `user_deleted_at`	TIMESTAMP	NULL
+-- tbl_user 테이블 생성
+CREATE TABLE IF NOT EXISTS tbl_user (
+                                        user_id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                                        user_name VARCHAR(100) NOT NULL,
+                                        user_email VARCHAR(255) NOT NULL UNIQUE,
+                                        user_password VARCHAR(255) NOT NULL,
+                                        user_birthday DATE NOT NULL,
+                                        user_nickname VARCHAR(50) NOT NULL,
+                                        user_profile_picture_url VARCHAR(500),
+                                        user_tier VARCHAR(20),
+                                        user_level INT,
+                                        user_created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                                        user_updated_at TIMESTAMP,
+                                        user_deleted_at TIMESTAMP
 );
