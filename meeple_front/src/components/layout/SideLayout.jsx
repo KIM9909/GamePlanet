74ff45294135323c7302d752cb72a7b7a4ca1908
강은수 <kes2450@naver.com>
2025-01-25 01:18:29 +0900

    💄 Design : 네브바 + 사이드바 예외 경로 추가
    1. 메인 페이지, 게임 플레이 페이지 (부루마불, 바퀴벌레포코)에서 네브바 + 사이드바 안 보이도록 수정

--- meeple_front/src/components/layout/SideLayout.jsx
+++ meeple_front/src/components/layout/SideLayout.jsx
@@ -5,7 +5,10 @@ import { useLocation, Link } from "react-router-dom";
 const SideLayout = ({ children }) => {
   const location = useLocation();
   const showSidebar = location.pathname !== "/";
+  const [isShowSide, setIsShowSide] = useState(false);
   const [activeLink, setActiveLink] = useState(location.pathname);
+
+  const hiddenPaths = ["/", "/game/burumabul", "/game/cockroachpoker"];
 
   const linkStyle =
     "text-white hover:text-[#D7C3F1] transition-colors duration-300 relative after:content-[''] after:absolute after:w-0 after:h-0.5 after:bg-[#9694FF] after:left-0 after:-bottom-1 hover:after:w-full after:transition-all after:duration-300";
@@ -14,11 +17,16 @@ const SideLayout = ({ children }) => {
 
   useEffect(() => {
     setActiveLink(location.pathname);
+    if (hiddenPaths.includes(activeLink)) {
+      setIsShowSide(false);
+    } else {
+      setIsShowSide(true);
+    }
   }, [location.pathname]);
 
   return (
     <div>
-      {showSidebar ? (
+      {isShowSide && showSidebar ? (
         <div style={{ userSelect: "none" }} className="h-screen">
           <div className="flex flex-row">
             <div className="flex w-1/5 bg-gradient-to-r from-gray-800 to-gray-800 h-screen border-2">
