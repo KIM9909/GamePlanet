6b46f4acab2cf1dd9b94195955c1b6282eca2aa9
강은수 <kes2450@naver.com>
2025-01-24 16:37:13 +0900

    ✨ Feat(home): 사이드바 생성
    1. 사이드바 생성
    2. 각 페이지로 이동 가능
    3. 레이아웃 기능을 이용하여 모든페이지에서 사이드바 보이도록 구현
    4. hover + text-shadow 기능으로 해당 페이지임을 확인할 수 있도록 표시
    
    Related to : #101

--- meeple_front/src/App.jsx
+++ meeple_front/src/App.jsx
@@ -13,49 +13,56 @@ import { Store } from "./sources/api/store/Store";
 import { Provider } from "react-redux";
 import MainPage from "./pages/main/MainPage";
 import TopLayout from "./components/layout/TopLayout";
+import SideLayout from "./components/layout/SideLayout";
+import Introduce from "./pages/introduce/Introduce";
 
 function App() {
   return (
     <Provider store={Store}>
       <BrowserRouter>
         <TopLayout>
-          <Routes>
-            {/* Admin */}
-            <Route path="/admin" element={<AdminPage />} />
+          <SideLayout>
+            <Routes>
+              {/* Admin */}
+              <Route path="/admin" element={<AdminPage />} />
 
-            {/* Board */}
-            <Route path="/board" element={<BoardPage />} />
-            <Route path="/board/:boardId" element={<BoardPage />} />
+              {/* Board */}
+              <Route path="/board" element={<BoardPage />} />
+              <Route path="/board/:boardId" element={<BoardPage />} />
 
-            {/* Game */}
-            <Route path="/game/burumabul" element={<BurumabulPage />} />
-            <Route
-              path="/game/cockroachpoker"
-              element={<CockroachPokerPage />}
-            />
+              {/* Game */}
+              <Route path="/game/burumabul" element={<BurumabulPage />} />
+              <Route
+                path="/game/cockroachpoker"
+                element={<CockroachPokerPage />}
+              />
 
-            {/* GameInfo */}
-            <Route path="/game/:gameId" element={<GameInfoPage />} />
+              {/* GameInfo */}
+              <Route path="/game/:gameId" element={<GameInfoPage />} />
 
-            {/* Home & Main */}
-            <Route path="/home" element={<HomePage />} />
-            <Route path="/" element={<MainPage />} />
+              {/* Home & Main */}
+              <Route path="/home" element={<HomePage />} />
+              <Route path="/" element={<MainPage />} />
 
-            {/* Profile */}
-            <Route path="/profile" element={<ProfilePage />} />
-            <Route path="/profile/:userId" element={<ProfilePage />} />
+              {/* Profile */}
+              <Route path="/profile" element={<ProfilePage />} />
+              <Route path="/profile/:userId" element={<ProfilePage />} />
 
-            {/* Proposal */}
-            <Route path="/proposal" element={<ProposalPage />} />
-            <Route path="/proposal/:proposalId" element={<ProposalPage />} />
+              {/* Proposal */}
+              <Route path="/proposal" element={<ProposalPage />} />
+              <Route path="/proposal/:proposalId" element={<ProposalPage />} />
 
-            {/* Tournament */}
-            <Route path="/tournament" element={<TournamentPage />} />
-            <Route
-              path="/tournament/:tournamentId"
-              element={<TournamentPage />}
-            />
-          </Routes>
+              {/* Tournament */}
+              <Route path="/tournament" element={<TournamentPage />} />
+              <Route
+                path="/tournament/:tournamentId"
+                element={<TournamentPage />}
+              />
+
+              {/* INTRODUCE */}
+              <Route path="/introduce" element={<Introduce />} />
+            </Routes>
+          </SideLayout>
         </TopLayout>
       </BrowserRouter>
     </Provider>
