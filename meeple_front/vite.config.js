a4279860dbe853df09ae126df4de2fe7980eb267
윤희준 <yhj0566@naer.com>
2025-01-24 14:22:32 +0900

    🐛 Fix : 방생성 완료 - roomId 받아옴 Resolved : #88

--- meeple_front/vite.config.js
+++ meeple_front/vite.config.js
@@ -1,19 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    global: 'globalThis',  // global 객체를 globalThis로 정의
+    global: "globalThis",
   },
   server: {
     port: 5173,
     proxy: {
-      '/ws': {
-        target: 'http://localhost:8090',  // 백엔드 서버 포트로 수정
+      "/ws": {
+        target: "http://localhost:8090",
         ws: true,
+      },
+      "/api": {                       // 추가
+        target: "http://localhost:8090" // API 프록시
       }
-    }
-  }
-})
+    },
+  },
+});
