808561f134e22cabf7b5e1f4bce8f071dea9c1bc
kimhongbeom <fkdldj48@naver.com>
2025-01-25 21:40:37 +0900

    🚑!HOTFIX: [FE] App.jsx / HomePage.jsx 수정

--- meeple_front/src/App.jsx
+++ meeple_front/src/App.jsx
@@ -18,52 +18,52 @@ function App() {
   // const { userId } = useSelector((state) => state.user);
 
   return (
+    <BrowserRouter>
+      <TopLayout>
+        <SideLayout>
+          <Routes>
+            {/* Admin */}
+            <Route path="/admin" element={<AdminPage />} />
 
-    <Provider store={Store}>
-        <BrowserRouter>
-          <TopLayout>
-            <SideLayout>
-              <Routes>
-                {/* Admin */}
-                <Route path="/admin" element={<AdminPage />} />
+            {/* Board */}
+            <Route path="/board" element={<BoardPage />} />
+            <Route path="/board/:boardId" element={<BoardPage />} />
 
-                {/* Board */}
-                <Route path="/board" element={<BoardPage />} />
-                <Route path="/board/:boardId" element={<BoardPage />} />
+            {/* Game */}
+            <Route path="/game/burumabul" element={<BurumabulPage />} />
+            <Route
+              path="/game/cockroach/:roomId"
+              element={<CockroachPokerPage />}
+            />
 
-                {/* Game */}
-                <Route path="/game/burumabul" element={<BurumabulPage />} />
-                <Route path="/game/cockroach/:roomId" element={<CockroachPokerPage />} />
+            {/* GameInfo */}
+            <Route path="/game/:gameId" element={<GameInfoPage />} />
 
-                {/* GameInfo */}
-                <Route path="/game/:gameId" element={<GameInfoPage />} />
+            {/* Home & Main */}
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/" element={<MainPage />} />
 
-                {/* Home & Main */}
-                <Route path="/home" element={<HomePage />} />
-                <Route path="/" element={<MainPage />} />
+            {/* Profile */}
+            <Route path="/profile/:userId" element={<ProfilePage />} />
 
-                {/* Profile */}
-                <Route path="/profile/:userId" element={<ProfilePage />} />
+            {/* Proposal */}
+            <Route path="/proposal" element={<ProposalPage />} />
+            <Route path="/proposal/:proposalId" element={<ProposalPage />} />
 
-                {/* Proposal */}
-                <Route path="/proposal" element={<ProposalPage />} />
-                <Route path="/proposal/:proposalId" element={<ProposalPage />} />
+            {/* Tournament */}
+            <Route path="/tournament" element={<TournamentPage />} />
+            <Route
+              path="/tournament/:tournamentId"
+              element={<TournamentPage />}
+            />
 
-                {/* Tournament */}
-                <Route path="/tournament" element={<TournamentPage />} />
-                <Route
-                  path="/tournament/:tournamentId"
-                  element={<TournamentPage />}
-                />
-
-                {/* INTRODUCE */}
-                <Route path="/introduce" element={<Introduce />} />
-              </Routes>
-            </SideLayout>
-          </TopLayout>
-        </BrowserRouter>
-      </Provider>
-      );
-    }
+            {/* INTRODUCE */}
+            <Route path="/introduce" element={<Introduce />} />
+          </Routes>
+        </SideLayout>
+      </TopLayout>
+    </BrowserRouter>
+  );
+}
 
 export default App;
