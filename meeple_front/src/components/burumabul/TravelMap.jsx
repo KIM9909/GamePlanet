0baa02f01fbe62998840d442e0b0e4e1055f6ba7
강은수 <kes2450@naver.com>
2025-01-27 03:13:20 +0900

    💄 Design(burumabul) : 보드판 셀 이미지 적용
    - 피그마로 이미지 생성 후 적용
    - 아직 구현 중

--- meeple_front/src/components/burumabul/TravelMap.jsx
+++ meeple_front/src/components/burumabul/TravelMap.jsx
@@ -12,7 +12,7 @@ import Dice from "./Dice";
 import { TextureLoader } from "three";
 import spaceBackground from "../../assets/burumabul_images/space.jpg";
 
-import earthTexture from "../../assets/burumabul_images/earth.jpg";
+import earthTexture from "../../assets/burumabul_images/earth.png";
 import marsTexture from "../../assets/burumabul_images/mars.jpg";
 
 const Cell = ({
@@ -381,14 +381,6 @@ const TravelMap = () => {
               maxPolarAngle={Math.PI / 2.5} // 위쪽으로 카메라 제한
               minDistance={5} // 최소 줌 거리
               maxDistance={15} // 최대 줌 거리
-              mouseButtons={{
-                LEFT: 0,
-                MIDDLE: 1,
-                RIGHT: 2,
-              }}
-              enablePan={false}
-              zoomToCursor={true}
-              rotateSpeed={0.18}
             />
             {renderCells()}
           </Canvas>
