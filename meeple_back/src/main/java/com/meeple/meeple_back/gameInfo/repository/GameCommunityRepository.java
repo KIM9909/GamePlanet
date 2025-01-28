7e8ccdc71d386e047c8f9b3db22dd21862a51dfb
kimhongbeom <fkdldj48@naver.com>
2025-01-28 22:46:53 +0900

    🐛Fix: Repository, 엔드포인트 중복 수정

--- meeple_back/src/main/java/com/meeple/meeple_back/gameInfo/repository/GameCommunityRepository.java
+++ meeple_back/src/main/java/com/meeple/meeple_back/gameInfo/repository/GameCommunityRepository.java
@@ -1,8 +1,9 @@
 package com.meeple.meeple_back.gameInfo.repository;
 
+import com.meeple.meeple_back.gameInfo.model.entity.GameCommunity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GameCommunityRepository extends JpaRepository<GameCommunityRepository, Integer> {
+public interface GameCommunityRepository extends JpaRepository<GameCommunity, Integer> {
 }
