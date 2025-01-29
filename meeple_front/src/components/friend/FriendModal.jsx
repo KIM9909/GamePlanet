60dd6ab76d830ea469713d6f24089e13c79fee06
강은수 <kes2450@naver.com>
2025-01-30 04:43:24 +0900

    ✨ Feat : 리덕스에 친구 관련 상태 추가

---
+++ meeple_front/src/components/friend/FriendModal.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { useModal } from "../context/ModalContext";
+
+const FriendModal = () => {
+  const { isOpen, setIsOpen } = useModal(); // 컨텍스트에서 상태 가져오기
+};
---
+++ meeple_front/src/components/layout/FriendModalLayout.jsx
@@ -0,0 +1,9 @@
+import React, { createContent, useContext, useState } from "react";
+
+const ModalContent = createContext();
+
+const FriendModalLayout = () => {
+  // const
+};
+
+export default FriendModalLayout;
