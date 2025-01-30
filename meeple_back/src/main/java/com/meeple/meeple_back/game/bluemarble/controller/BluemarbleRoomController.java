bbad26639a7dc0c09e93213b88939bdbd0eae1ca
seonghyunKim <aruesin2@gmail.com>
2025-01-31 01:45:40 +0900

    :sparkles: Feat(blue-marble) : 1. 브루마불 게임방 참가 2. Room Class에 Player 저장 자료구조를 List로 변경 3. Room 존재 하지 않는경우 예외 처리 4. 에러 처리 AOP 추가 5. JWTUtil -> JWT 토큰에서 userId 추출하는 기능 추가
    Related to : #290

--- meeple_back/src/main/java/com/meeple/meeple_back/game/bluemarble/controller/BluemarbleRoomController.java
+++ meeple_back/src/main/java/com/meeple/meeple_back/game/bluemarble/controller/BluemarbleRoomController.java
@@ -3,6 +3,7 @@ package com.meeple.meeple_back.game.bluemarble.controller;
 import com.meeple.meeple_back.game.bluemarble.controller.port.BluemarbleRoomService;
 import com.meeple.meeple_back.game.bluemarble.controller.response.RoomResponse;
 import com.meeple.meeple_back.game.bluemarble.domain.RoomCreate;
+import com.meeple.meeple_back.util.JwtUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -10,7 +11,9 @@ import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,10 +26,20 @@ public class BluemarbleRoomController {
 
 	private final BluemarbleRoomService bluemarbleRoomService;
 
+	private final JwtUtil jwtUtil;
+
 	@PostMapping
 	@Operation(summary = "게임방 생성", description = "새로운 블루마블 게임방을 생성합니다.")
 	public ResponseEntity<RoomResponse> create(@Valid RoomCreate roomCreate) {
 		return ResponseEntity.status(HttpStatus.CREATED)
 				.body(RoomResponse.from(bluemarbleRoomService.create(roomCreate)));
+	}
+
+	@PostMapping("/{roomId}")
+	@Operation(summary = "게임방 참가", description = "게임방에 참가합��다.")
+	public ResponseEntity<RoomResponse> join(@PathVariable int roomId,
+			@RequestHeader("Authorization") String token) {
+		long userId = jwtUtil.getUserIdFromToken(token);
+		return ResponseEntity.ok(RoomResponse.from(bluemarbleRoomService.join(roomId, userId)));
 	}
 }
--- meeple_back/src/main/java/com/meeple/meeple_back/game/bluemarble/controller/port/BluemarbleRoomService.java
+++ meeple_back/src/main/java/com/meeple/meeple_back/game/bluemarble/controller/port/BluemarbleRoomService.java
@@ -6,4 +6,6 @@ import com.meeple.meeple_back.game.bluemarble.domain.RoomCreate;
 public interface BluemarbleRoomService {
 
 	Room create(RoomCreate roomCreate);
+	
+	Room join(int roomId, long userId);
 }
