4efb7bb9b9a980d8927062c678181b5936ba15e4
yunjaeeun <yunjaeeun12@naver.com>
2025-01-31 20:25:24 +0900

    📝 Docs: Swagger
    1. 바퀴벌레 포커 API 문서 추가
    2. 캐치마인드 API 문서 추가
    3. OpenVidu API 문서 추가

--- meeple_back/src/main/java/com/meeple/meeple_back/game/cockroach/model/response/ResponseExitRoom.java
+++ meeple_back/src/main/java/com/meeple/meeple_back/game/cockroach/model/response/ResponseExitRoom.java
@@ -1,5 +1,6 @@
 package com.meeple.meeple_back.game.cockroach.model.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
@@ -8,5 +9,6 @@ import java.util.List;
 @Data
 @Builder
 public class ResponseExitRoom {
+    @Schema(description = "참가자 목록", example = "['nick1', 'nick2']")
     List<String> players;
 }
--- meeple_back/src/main/java/com/meeple/meeple_back/gameInfo/model/response/ResponseUpdateGameInfo.java
+++ meeple_back/src/main/java/com/meeple/meeple_back/gameInfo/model/response/ResponseUpdateGameInfo.java
@@ -1,11 +1,15 @@
 package com.meeple.meeple_back.gameInfo.model.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 public class ResponseUpdateGameInfo {
+    @Schema(description = "응답코드", example = "200")
     int code;
+
+    @Schema(description = "응답메세지", example = "정상작동")
     String message;
 
     public ResponseUpdateGameInfo(int code, String message) {
