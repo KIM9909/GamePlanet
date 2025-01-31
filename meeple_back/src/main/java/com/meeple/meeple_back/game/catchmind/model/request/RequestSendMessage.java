e950083f4d8c9454230d754c8a84be374e1173bf
yunjaeeun <yunjaeeun12@naver.com>
2025-01-31 20:37:34 +0900

    📝 Docs: Swagger
    1. 캐치마인드 API 문서 추가

--- meeple_back/src/main/java/com/meeple/meeple_back/game/catchmind/model/request/RequestSendMessage.java
+++ meeple_back/src/main/java/com/meeple/meeple_back/game/catchmind/model/request/RequestSendMessage.java
@@ -1,10 +1,14 @@
 package com.meeple.meeple_back.game.catchmind.model.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
 @Data
 public class RequestSendMessage {
+    @Schema(description = "메세지", example = "메세지", required = true)
     String message;
+    @Schema(description = "발송자", example = "nick1", required = true)
     String sender;
+    @Schema(description = "정답", example = "사과", required = true)
     String correctAnswer;
 }
--- meeple_back/src/main/java/com/meeple/meeple_back/game/catchmind/model/response/ResponseCreateRoom.java
+++ meeple_back/src/main/java/com/meeple/meeple_back/game/catchmind/model/response/ResponseCreateRoom.java
@@ -1,10 +1,12 @@
 package com.meeple.meeple_back.game.catchmind.model.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 public class ResponseCreateRoom {
+    @Schema(description = "게임방 Pk", example = "1", required = true)
     private int roomId;
 }
