b726ceda41c8cc49471397bd65defb75505fd22d
seonghyunKim <aruesin2@gmail.com>
2025-02-03 22:56:41 +0900

    :sparkles: Feat(bluemable) :
    1. 방 생성 기능 구현
    2. 주사위 굴리기
    3. asyncapi.yml 작성
    
    related: #324
    
    Related to : #314

--- meeple_back/src/main/java/com/meeple/meeple_back/game/bluemarble/controller/BluemarbleGameController.java
+++ meeple_back/src/main/java/com/meeple/meeple_back/game/bluemarble/controller/BluemarbleGameController.java
@@ -11,10 +11,10 @@ import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @Tag(name = "게임플레이(블루마블)")
 @Builder
 @RequiredArgsConstructor
