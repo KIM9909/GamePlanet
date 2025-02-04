5d37a2dee947a521065b3147087eb95e09cc46e9
유진혁 <hiu05043@naver.com>
2025-02-04 17:58:39 +0900

    ✨ Feat: 게임정보 페이지 컴포넌트 내용생성

--- meeple_front/src/components/info/GameReview.jsx
+++ meeple_front/src/components/info/GameReview.jsx
@@ -1,11 +1,26 @@
 import ReviewList from "./ReviewList";
+import { useParams } from "react-router-dom";
+import { useEffect,useState } from "react";
+import axios from "axios";
 
 const GameReview = () =>{
+  const params= useParams();
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    axios.get(`https://boardjjigae.duckdns.org/api/game-info/review`)
+      .then(response => setData(response.data))
+      .catch(error => console.error(error));
+  }, [params]);
+
   return (
     <div>
+      {/* 평균별점 */}
+      <section>{data.starAvg}</section>
       {/* 리뷰목록 */}
       <section>
-        <ReviewList></ReviewList>
+        리뷰목록
       </section>
     </div>
   )
