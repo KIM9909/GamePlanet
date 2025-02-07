1f202d575305463d305d791b0abaa541be0d8120
유진혁 <hiu05043@naver.com>
2025-02-07 17:59:53 +0900

    ✨ Feat : 게임 정보 페이지 추가

--- meeple_front/src/pages/gameInfo/GameInfoPage.jsx
+++ meeple_front/src/pages/gameInfo/GameInfoPage.jsx
@@ -1,43 +1,82 @@
-import { useState } from "react"
-import TopNavbar from "../../components/Navbar/TopNavBar"
-import InfoSideBar from "../../components/info/InfoSideBar"
-import GameInfo from "../../components/info/GameInfo"
-import GameRule from "../../components/info/GameRule"
-import GameCommunity from "../../components/info/GameCommunity"
-import ArticleList from "../../components/info/ArticleList"
-import ReviewList from "../../components/info/ReviewList"
-import PlayVideo from "../../components/info/PlayVideo"
+// import { useState } from "react"
+// import TopNavbar from "../../components/Navbar/TopNavBar"
+// import InfoSideBar from "../../components/info/InfoSideBar"
+// import GameInfo from "../../components/info/GameInfo"
+// import GameRule from "../../components/info/GameRule"
+// import GameCommunity from "../../components/info/GameCommunity"
+// import ArticleList from "../../components/info/ArticleList"
+// import ReviewList from "../../components/info/ReviewList"
+// import PlayVideo from "../../components/info/PlayVideo"
+
+// const GameInfoPage = () => {
+//   const [selectedMenu, setSelectedMenu] = useState("gameinfo")
+
+//   const renderContent = () => {
+//     switch(selectedMenu) {
+//       case "gameinfo":
+//         return <GameInfo />
+//       case "gamerule":
+//         return <GameRule />
+//       case "gamecommunity":
+//         return <GameCommunity />
+//       case "review":
+//         return <ReviewList />
+//       case "playvideo":
+//         return <PlayVideo />
+//       default:
+//         return <GameInfo />
+//     }
+//   }
+
+//   return (
+//     <>
+//       <div className="flex">
+//         <InfoSideBar onMenuSelect={setSelectedMenu} />
+//         <div className="content flex-1 mx-10px bg-gray-200">
+//           {renderContent()}
+//         </div>
+//       </div>
+//     </>
+//   )
+// };
+
+// export default GameInfoPage;
+
+
+
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 const GameInfoPage = () => {
-  const [selectedMenu, setSelectedMenu] = useState("gameinfo")
+  const { gameId } = useParams(); // URL에서 gameId 받아오기
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const renderContent = () => {
-    switch(selectedMenu) {
-      case "gameinfo":
-        return <GameInfo />
-      case "gamerule":
-        return <GameRule />
-      case "gamecommunity":
-        return <GameCommunity />
-      case "review":
-        return <ReviewList />
-      case "playvideo":
-        return <PlayVideo />
-      default:
-        return <GameInfo />
-    }
-  }
+  useEffect(() => {
+    setLoading(true);
+    axios.get(`https://boardjjigae.duckdns.org/api/game-info/${gameId}`) //받아온 gameId로 게임 정보 받아오기
+      .then(response => {
+        setData(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+        setLoading(false);
+      });
+  }, [gameId]);
+
+  if (loading) return <div>로딩중...</div>; // 정보 받기 전에 표시시
+  if (error) return <div>에러가 발생했습니다.</div>;  //에러 발생 시
+  if (!data) return <div>데이터가 없습니다.</div>;  
 
   return (
-    <>
-      <div className="flex">
-        <InfoSideBar onMenuSelect={setSelectedMenu} />
-        <div className="content flex-1 mx-10px bg-gray-200">
-          {renderContent()}
-        </div>
-      </div>
-    </>
-  )
+    <div>
+      <section>{data.game.gameName}</section>
+      <section>{data.gameInfoContent}</section>
+    </div>
+  );
 };
 
 export default GameInfoPage;
