aeaf2b00774768105a95536dcbfdf2e573e8635d
유진혁 <hiu05043@naver.com>
2025-02-09 21:22:15 +0900

    ✨ Feat : 게시글 수정

--- meeple_front/src/pages/gameInfo/board/EditArticlePage.jsx
+++ meeple_front/src/pages/gameInfo/board/EditArticlePage.jsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import axios from 'axios';
 import BoardHeader from "../../../components/info/BoardHeader";
 import ArticleForm from "../../../components/info/ArticleForm";
+import { GameInfoAPI } from '../../../sources/api/GameInfoAPI';
 
 const EditArticlePage = () => {
-  const { gameId,articleId } = useParams();
+  const { gameId, articleId } = useParams();
   const navigate = useNavigate();
   const [article, setArticle] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
@@ -13,28 +13,25 @@ const EditArticlePage = () => {
   useEffect(() => {
     const fetchArticle = async () => {
       try {
-        const response = await axios.get(`/game-info/community/${articleId}`);
-        setArticle(response.data);
+        const response = await GameInfoAPI.getCommunityPost(articleId);
+        setArticle(response);
       } catch (error) {
         console.error('게시글 조회 실패:', error);
         alert('게시글을 불러오는데 실패했습니다.');
-        navigate('/board');
+        navigate(`/game/${gameId}/board`);
       } finally {
         setIsLoading(false);
       }
     };
 
     fetchArticle();
-  }, [articleId, navigate]);
+  }, [articleId, gameId, navigate]);
 
   const handleSubmit = async (formData) => {
     try {
-      const response = await axios.put(`/game-info/community/${articleId}`, formData);
-      
-      if (response.status === 200) {
-        alert('게시글이 수정되었습니다.');
-        navigate(`game/${gameId}/board/detail/${articleId}`); // 수정된 게시글 상세 페이지로 이동
-      }
+      await GameInfoAPI.updateCommunityPost(articleId, formData);
+      alert('게시글이 수정되었습니다.');
+      navigate(`/game/${gameId}/board/detail/${articleId}`);
     } catch (error) {
       console.error('게시글 수정 실패:', error);
       throw error;
@@ -57,6 +54,7 @@ const EditArticlePage = () => {
       <section>
         {article && (
           <ArticleForm 
+            gameId={gameId}
             initialData={article}
             onSubmit={handleSubmit}
             isEditing={true}
