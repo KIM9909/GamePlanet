73500c7489b3143a111f02fbddf171427f560306
seonghyunKim <aruesin2@gmail.com>
2025-02-13 02:10:07 +0900

    :sparkles: : Feat(custom) :
    1. 타일 이미지 S3에 업로드하고 image URL 받아서 Tile 테이블에 저장
    
    related: #458

--- meeple_back/src/main/java/com/meeple/meeple_back/gameCustom/bluemarble/controller/CustomTileController.java
+++ meeple_back/src/main/java/com/meeple/meeple_back/gameCustom/bluemarble/controller/CustomTileController.java
@@ -1,38 +1,58 @@
 package com.meeple.meeple_back.gameCustom.bluemarble.controller;
 
-import com.meeple.meeple_back.game.bluemarble.domain.Tile;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.meeple.meeple_back.gameCustom.bluemarble.controller.request.CustomTileRequest;
 import com.meeple.meeple_back.gameCustom.bluemarble.controller.response.CustomTileResponse;
 import com.meeple.meeple_back.gameCustom.bluemarble.controller.response.TileResponse;
 import com.meeple.meeple_back.gameCustom.bluemarble.infrastructure.CustomTileId;
 import com.meeple.meeple_back.gameCustom.bluemarble.service.CustomTileService;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController("/custom-element/tile")
 @RequestMapping
 @RequiredArgsConstructor
 public class CustomTileController {
-	// 특정 커스텀의 타일 정보를 조회, 생성, 업데이트, 삭제할 수 있따. 조회할 수 있다.
-	// pk -> CustomId, TileId
 	private final CustomTileService customTileService;
+	private final AmazonS3 amazonS3;
+
+	@Value("${aws.s3.bucket}")
+	private String bucketName;
 
 	// CREATE
-	@PostMapping("/{customId}")
-	public ResponseEntity<CustomTileResponse>  createCustomTile(@PathVariable Integer customId,
-			@RequestBody CustomTileRequest request) {
-		CustomTileResponse response= customTileService.create(request);
-		return ResponseEntity.ok(response);
+	@PostMapping(value = "/{customId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<?> createCustomTile(@PathVariable Integer customId,
+	                                          @RequestParam("tileName") String tileName, @RequestParam("tileNumber") Integer tileNumber, @RequestParam("tileType") String tileType, @RequestParam("tilePrice") Integer tilePrice, @RequestPart("tileImage") MultipartFile tileImage) {
+		if (tileImage.isEmpty()) {
+			return ResponseEntity.badRequest().body("파일이 전송되지 않았습니다.");
+		}
+		if (!"image/png".equals(tileImage.getContentType())) {
+			return ResponseEntity.badRequest().body("PNG 이미지 형식만 지원합니다.");
+		}
+		try {
+			String fileName = "custom-tile-" + System.currentTimeMillis() + ".png";
+
+			ObjectMetadata metadata = new ObjectMetadata();
+			metadata.setContentType(tileImage.getContentType());
+			metadata.setContentLength(tileImage.getSize());
+
+			amazonS3.putObject(bucketName, fileName, tileImage.getInputStream(), metadata);
+
+			String fileUrl = amazonS3.getUrl(bucketName, fileName).toString();
+
+			CustomTileResponse response = customTileService.create(CustomTileRequest.builder().customId(customId).tileName(tileName).tileNumber(tileNumber).tileType(tileType).tileImageUrl(fileUrl).tilePrice(tilePrice).build());
+			return ResponseEntity.ok(response);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
 	}
 
 	// READ ALL
@@ -45,19 +65,19 @@ public class CustomTileController {
 	// READ ONE
 	@GetMapping("/{customId}/read/{tileId}")
 	public ResponseEntity<CustomTileResponse> getCustomTile(@PathVariable Integer customId,
-			@PathVariable Integer tileId) {
+	                                                        @PathVariable Integer tileId) {
 		CustomTileId id = new CustomTileId(customId, tileId);
-		CustomTileResponse response= customTileService.findById(id);
+		CustomTileResponse response = customTileService.findById(id);
 		return ResponseEntity.ok(response);
 	}
 
 	// UPDATE
 	@PutMapping("/{customId}/update/{tileId}")
 	public ResponseEntity<CustomTileResponse> updateCustomTile(@PathVariable Integer customId,
-			@PathVariable Integer tileId,
-			@RequestBody CustomTileRequest request) {
+	                                                           @PathVariable Integer tileId,
+	                                                           @RequestBody CustomTileRequest request) {
 		CustomTileId id = new CustomTileId(customId, tileId);
-		CustomTileResponse response =  customTileService.update(id, request);
+		CustomTileResponse response = customTileService.update(id, request);
 
 		return ResponseEntity.ok(response);
 	}
@@ -65,7 +85,7 @@ public class CustomTileController {
 	// DELETE
 	@DeleteMapping("/{customId}/delete/{tileId}")
 	public ResponseEntity<Void> deleteCustomTile(@PathVariable Integer customId,
-			@PathVariable Integer tileId) {
+	                                             @PathVariable Integer tileId) {
 		CustomTileId id = new CustomTileId(customId, tileId);
 		customTileService.delete(id);
 		return ResponseEntity.noContent().build();
