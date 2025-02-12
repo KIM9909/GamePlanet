a9585e2fd144481ee226143268d33ec1483f7f5f
seonghyun-kim <aruesin2@gmail.com>
2025-02-12 16:10:48 +0900

    Revert ":sparkles: Feat(custom)"
    This reverts commit c36e665f05af646a1b0feb43a046771cd1b234ff.

--- meeple_back/src/main/java/com/meeple/meeple_back/gameCustom/bluemarble/infrastructure/CardJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.meeple.meeple_back.gameCustom.bluemarble.infrastructure;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CardJpaRepository extends JpaRepository<CardEntity, Integer> {
-
-}
--- meeple_back/src/main/java/com/meeple/meeple_back/gameCustom/bluemarble/infrastructure/CustomCardId.java
+++ meeple_back/src/main/java/com/meeple/meeple_back/gameCustom/bluemarble/infrastructure/CustomCardId.java
@@ -2,7 +2,6 @@ package com.meeple.meeple_back.gameCustom.bluemarble.infrastructure;
 
 import jakarta.persistence.Embeddable;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,7 +9,6 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class CustomCardId implements java.io.Serializable {
 	private Integer customId;
 	private Integer cardId;
