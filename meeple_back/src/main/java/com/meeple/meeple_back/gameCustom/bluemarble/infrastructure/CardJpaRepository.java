7e64d988339c92951f12b8434e09117ed29a5939
seonghyunKim <aruesin2@gmail.com>
2025-02-14 02:12:16 +0900

    :sparkles: : Feat(bluemarble) :
    custom card 기능 추가
    
    related: #458

--- meeple_back/src/main/java/com/meeple/meeple_back/gameCustom/bluemarble/infrastructure/CardJpaRepository.java
+++ meeple_back/src/main/java/com/meeple/meeple_back/gameCustom/bluemarble/infrastructure/CardJpaRepository.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 
 @Repository
 public interface CardJpaRepository extends JpaRepository<CardEntity, Integer> {
-	@Query("select c from CardEntity c where c.cardNumber = :tileNumber and c.cardNumber IN (select t.tileEntity.tileNumber from CustomTileEntity t where t.customElement.customId = :customId)")
+	@Query("select c from CardEntity c where c.cardId IN (select t.id.cardId from CustomCardEntity t where t.customElement.customId = :customId) AND c.cardNumber = :tileNumber")
 	Optional<CardEntity> findByCustomIdAndCardNumber(int customId, Integer tileNumber);
 
 }
