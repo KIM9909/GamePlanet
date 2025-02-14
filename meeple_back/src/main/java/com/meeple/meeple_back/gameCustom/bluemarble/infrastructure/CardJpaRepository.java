414ab861e2b8169f1eb629fb0d070872f7f44a80
seonghyun-kim <aruesin2@gmail.com>
2025-02-14 12:19:42 +0900

    :sparkles: Feat(bluemarble)
    1. 타일 카드 업데이트 기능 추가
    
    related: #456

--- meeple_back/src/main/java/com/meeple/meeple_back/gameCustom/bluemarble/infrastructure/CardJpaRepository.java
+++ meeple_back/src/main/java/com/meeple/meeple_back/gameCustom/bluemarble/infrastructure/CardJpaRepository.java
@@ -11,4 +11,6 @@ public interface CardJpaRepository extends JpaRepository<CardEntity, Integer> {
 	@Query("select c from CardEntity c where c.cardId IN (select t.id.cardId from CustomCardEntity t where t.customElement.customId = :customId) AND c.cardNumber = :tileNumber")
 	Optional<CardEntity> findByCustomIdAndCardNumber(int customId, Integer tileNumber);
 
+	@Query("select c from CardEntity c where c.cardNumber= :number AND c.cardId IN (select cc.card.cardId from CustomCardEntity cc where cc.customElement.customId = :customId)")
+	CardEntity findByCardNumberAndCustomId(int customId, int number);
 }
