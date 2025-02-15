f0f4d684fa4bc9ddaa695023de47ecda35e9cd36
seonghyunKim <aruesin2@gmail.com>
2025-02-16 01:36:21 +0900

    :sparkles: Feat(custom)
    완성된 custom 요소 번호 목록 반환

--- meeple_back/src/main/java/com/meeple/meeple_back/gameCustom/bluemarble/controller/response/CustomElementListResponse.java
+++ meeple_back/src/main/java/com/meeple/meeple_back/gameCustom/bluemarble/controller/response/CustomElementListResponse.java
@@ -1,9 +1,11 @@
 package com.meeple.meeple_back.gameCustom.bluemarble.controller.response;
 
 import java.util.List;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class CustomElementListResponse {
     private int customId;
     private List<Integer> customCompleteList;
