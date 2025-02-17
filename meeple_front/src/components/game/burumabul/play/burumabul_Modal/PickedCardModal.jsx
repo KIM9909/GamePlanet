fdceb18b38703df8e2836d66fd88679b5a6e85ff
강은수 <kes2450@naver.com>
2025-02-18 00:23:17 +0900

    💄 Design : 특수카드 디자인 수정

--- meeple_front/src/components/game/burumabul/play/burumabul_Modal/PickedCardModal.jsx
+++ meeple_front/src/components/game/burumabul/play/burumabul_Modal/PickedCardModal.jsx
@@ -22,7 +22,7 @@ const PickedCardModal = ({ onClose, cardInfo }) => {
         <>
           <motion.div
             key={cardInfo.id}
-            className="relative bg-white w-96 h-80 p-4 rounded-lg"
+            className="relative bg-gray-900 bg-opacity-80 w-96 h-80 p-4 rounded-lg"
           >
             <motion.div
               className="relative w-full h-full cursor-pointer"
@@ -39,7 +39,7 @@ const PickedCardModal = ({ onClose, cardInfo }) => {
                   backgroundImage: `url(${drawCardBg})`,
                 }}
               >
-                <h2 className="text-2xl text-white font-bold">
+                <h2 className="text-2xl text-cyan-500 font-bold">
                   {cardInfo.name}
                 </h2>
                 <p className="text-gray-400 mt-8">Click!</p>
@@ -47,18 +47,17 @@ const PickedCardModal = ({ onClose, cardInfo }) => {
 
               {/* 뒷면 */}
               <motion.div
-                className="absolute w-full  h-full bg-gradient-to-br bg-[${cardInfo.color}] rounded-xl p-6 flex flex-col gap-4 items-center justify-center shadow-xl"
+                className="absolute w-full h-full bg-gray-900 border-2 border-cyan-400 rounded-xl p-6 flex flex-col gap-4 items-center justify-center shadow-xl"
                 style={{
                   backfaceVisibility: "hidden",
                   transform: "rotateY(180deg)",
-                  backgroundColor: "#A9B5DF",
                 }}
               >
-                <div className={`bg-[${cardInfo.color}] p-3 rounded`}>
+                <div className="p-3 rounded flex flex-col justify-center items-center">
                   <div className="mt-3">
                     <p className="text-white">{cardInfo.description}</p>
                   </div>
-                  <div className="mt-2 w-full text-gray-400">
+                  <div className="mt-4 w-20 h-8 flex justify-center items-center text-white rounded-lg bg-cyan-400 hover:bg-cyan-700">
                     <button onClick={onClose}>확인</button>
                   </div>
                 </div>
