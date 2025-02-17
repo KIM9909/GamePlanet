4055285d84452282a0151ca5edb1c3d5cee57440
kimhongbeom <fkdldj48@naver.com>
2025-02-17 20:51:37 +0900

    ✨Feat : 사이드 바 UI 수정 및 테트리스 게임 추가

--- meeple_front/src/components/layout/SideLayout.jsx
+++ meeple_front/src/components/layout/SideLayout.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useLocation, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import MiniTetris from "./MiniTetris";
 
 const SideLayout = ({ children }) => {
   const { token } = useSelector((state) => state.user);
@@ -11,10 +12,10 @@ const SideLayout = ({ children }) => {
   const notShowSidebar = !location.pathname.includes("/game-info");
   const [activeLink, setActiveLink] = useState(location.pathname);
 
-  const linkStyle =
-    "text-white hover:text-[#D7C3F1] transition-colors duration-300 relative after:content-[''] after:absolute after:w-0 after:h-0.5 after:bg-[#9694FF] after:left-0 after:-bottom-1 hover:after:w-full after:transition-all after:duration-300";
-
-  const textShadow = "0 0 5px #9694FF, 0 0 10px #9694FF, 0 0 20px #EBEAFF";
+  const linkStyle = (isActive) =>
+    `${
+      isActive ? "text-cyan-400" : "text-white"
+    } hover:text-cyan-300 transition-colors duration-300 relative after:content-[''] after:absolute after:w-0 after:h-0.5 after:bg-cyan-400 after:left-0 after:-bottom-1 hover:after:w-full after:transition-all after:duration-300`;
 
   useEffect(() => {
     setActiveLink(location.pathname);
@@ -27,108 +28,82 @@ const SideLayout = ({ children }) => {
           <div className="flex flex-row">
             <div className="flex w-2/7 bg-gradient-to-r from-gray-900/60 to-gray-900/60 h-screen border border-2 border-cyan-500/60">
               <div className="flex-1 m-2 border-2 box-border border-cyan-500/60">
-                <div className="flex flex-col m-2 text-center py-2 text-[33px] space-y-[40px]">
-                  {/* GAME INFO */}
-                  <div className="m-2">
-                    <Link
-                      to={`/game-info/${gameInfo.gameInfoId}`}
-                      className={linkStyle}
-                      onClick={() =>
-                        setActiveLink(`/game-info/${gameInfo.gameInfoId}`)
-                      }
-                      state={{ gameInfo }}
-                      style={
-                        activeLink === `/game-info/${gameInfo.gameInfoId}`
-                          ? {
-                              color: "#D7C3F1",
-                              textShadow: textShadow,
-                            }
-                          : {
-                              textShadow: textShadow,
-                            }
-                      }
-                    >
-                      GAME INFO
-                    </Link>
+                <div className="flex flex-col h-full">
+                  {/* Navigation Links */}
+                  <div className="flex flex-col text-center py-6 space-y-8 text-[33px]">
+                    <div>
+                      <Link
+                        to={`/game-info/${gameInfo.gameInfoId}`}
+                        className={linkStyle(
+                          activeLink === `/game-info/${gameInfo.gameInfoId}`
+                        )}
+                        onClick={() =>
+                          setActiveLink(`/game-info/${gameInfo.gameInfoId}`)
+                        }
+                        state={{ gameInfo }}
+                      >
+                        GAME INFO
+                      </Link>
+                    </div>
+
+                    <div>
+                      <Link
+                        to={`/game-info/${gameInfo.gameInfoId}/board`}
+                        className={linkStyle(
+                          activeLink ===
+                            `/game-info/${gameInfo.gameInfoId}/board`
+                        )}
+                        onClick={() =>
+                          setActiveLink(
+                            `/game-info/${gameInfo.gameInfoId}/board`
+                          )
+                        }
+                        state={{ gameInfo }}
+                      >
+                        COMMUNITY
+                      </Link>
+                    </div>
+
+                    <div>
+                      <Link
+                        to={`/game-info/${gameInfo.gameInfoId}/review`}
+                        className={linkStyle(
+                          activeLink ===
+                            `/game-info/${gameInfo.gameInfoId}/review`
+                        )}
+                        onClick={() =>
+                          setActiveLink(
+                            `/game-info/${gameInfo.gameInfoId}/review`
+                          )
+                        }
+                        state={{ gameInfo }}
+                      >
+                        GAME REVIEW
+                      </Link>
+                    </div>
+
+                    <div>
+                      <Link
+                        to={`/game-info/${gameInfo.gameInfoId}/custom`}
+                        className={linkStyle(
+                          activeLink ===
+                            `/game-info/${gameInfo.gameInfoId}/custom`
+                        )}
+                        onClick={() =>
+                          setActiveLink(
+                            `/game-info/${gameInfo.gameInfoId}/custom`
+                          )
+                        }
+                        state={{ gameInfo }}
+                      >
+                        GAME CUSTOM
+                      </Link>
+                    </div>
                   </div>
 
-                  {/* COMMUNITY */}
-                  <div className="m-2">
-                    <Link
-                      to={`/game-info/${gameInfo.gameInfoId}/board`}
-                      className={linkStyle}
-                      onClick={() =>
-                        setActiveLink(`/game-info/${gameInfo.gameInfoId}/board`)
-                      }
-                      state={{ gameInfo }}
-                      style={
-                        activeLink === `/game-info/${gameInfo.gameInfoId}/board`
-                          ? {
-                              color: "#D7C3F1",
-                              textShadow: textShadow,
-                            }
-                          : {
-                              color: "white",
-                              textShadow: textShadow,
-                            }
-                      }
-                    >
-                      COMMUNITY
-                    </Link>
-                  </div>
-
-                  {/* GAME REVIEW */}
-                  <div className="m-2">
-                    <Link
-                      to={`/game-info/${gameInfo.gameInfoId}/review`}
-                      className={linkStyle}
-                      onClick={() =>
-                        setActiveLink(
-                          `/game-info/${gameInfo.gameInfoId}/review`
-                        )
-                      }
-                      state={{ gameInfo }}
-                      style={
-                        activeLink ===
-                        `/game-info/${gameInfo.gameInfoId}/review`
-                          ? {
-                              color: "#D7C3F1",
-                              textShadow: textShadow,
-                            }
-                          : {
-                              textShadow: textShadow,
-                            }
-                      }
-                    >
-                      GAME REVIEW
-                    </Link>
-                  </div>
-
-                  {/* CUSTOM */}
-                  <div className="m-2">
-                    <Link
-                      to={`/game-info/${gameInfo.gameInfoId}/custom`}
-                      className={linkStyle}
-                      onClick={() =>
-                        setActiveLink(
-                          `/game-info/${gameInfo.gameInfoId}/custom`
-                        )
-                      }
-                      state={{ gameInfo }}
-                      style={
-                        activeLink ===
-                        `/game-info/${gameInfo.gameInfoId}/custom`
-                          ? {
-                              color: "#D7C3F1",
-                              textShadow: textShadow,
-                            }
-                          : {
-                              textShadow: textShadow,
-                            }
-                      }
-                    >
-                      GAME CUSTOM
-                    </Link>
+                  {/* Mini Tetris */}
+                  <div className="mt-auto mb-20">
+                    <MiniTetris />
                   </div>
                 </div>
               </div>
