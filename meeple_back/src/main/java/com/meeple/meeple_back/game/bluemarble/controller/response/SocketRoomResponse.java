9939b629924da2ee12d4df470ca7b35c4a553f44
seonghyun-kim <aruesin2@gmail.com>
2025-02-17 09:45:14 +0900

    :sparkles: Feat(bluemarble)
    1. 대기방 요청 response에 적용가능한 커스텀 목록 추가
    
    related: #511

--- meeple_back/src/main/java/com/meeple/meeple_back/game/bluemarble/controller/response/SocketRoomResponse.java
+++ meeple_back/src/main/java/com/meeple/meeple_back/game/bluemarble/controller/response/SocketRoomResponse.java
@@ -1,15 +1,20 @@
 package com.meeple.meeple_back.game.bluemarble.controller.response;
 
+import com.meeple.meeple_back.gameCustom.bluemarble.controller.response.CustomElementResponse;
+import java.util.List;
 import lombok.Builder;
+import lombok.Data;
 import lombok.Getter;
 
 @Builder
 @Getter
+@Data
 public class SocketRoomResponse {
 
 	private String type;
 	private RoomResponse roomResponse;
 	private String message;
+	private List<CustomElementResponse> customElementResponses;
 
 	public static SocketRoomResponse of(RoomResponse roomResponse, String message) {
 		return SocketRoomResponse.builder()
--- meeple_back/src/main/java/com/meeple/meeple_back/game/bluemarble/domain/GamePlayCreate.java
+++ meeple_back/src/main/java/com/meeple/meeple_back/game/bluemarble/domain/GamePlayCreate.java
@@ -16,11 +16,14 @@ public class GamePlayCreate {
 	@NotNull
 	private List<Integer> playerIds;
 
+	private int customId;
+
 	@JsonCreator
 	@Builder
 	public GamePlayCreate(@JsonProperty("gamePlayId") int gamePlayId,
-			@JsonProperty("players") List<Integer> playerIds) {
+			@JsonProperty("players") List<Integer> playerIds, @JsonProperty("customId") int customId) {
 		this.gamePlayId = gamePlayId;
 		this.playerIds = playerIds;
+		this.customId = customId;
 	}
 }
